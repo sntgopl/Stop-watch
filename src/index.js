c0f6e677a82af4d1f7312125babe2b0eacc6d535
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-17 23:40:07 -0300

    Add start button event with correct values

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import _ from 'lodash';
+import { hoursSet, minutesSet, secondsSet, startBtn } from './modules/selectors';
+import start from './modules/stopwatch';
 import './style.css';
+
+startBtn.addEventListener('click', (e) => {
+  start(hoursSet.value, minutesSet.value, secondsSet.value);
+  console.log('hi');
+});
